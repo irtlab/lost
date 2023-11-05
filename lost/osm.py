1cbf209939ceb6cbd6c1d10d338a1368c7853d80
Jan Janak <jan@janakj.org>
2023-11-05 13:19:24 -0500

    Update polygons downloaded from OSM

--- lost/osm.py
+++ lost/osm.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import json
 import requests
@@ -5,63 +6,94 @@ import osm2geojson
 from contextlib import suppress
 from os.path import join, realpath, dirname
 
-NOMINATIM_API = 'https://nominatim.openstreetmap.org'
-OVERPASS_API = 'https://overpass-api.de/api/interpreter'
+NOMINATIM_API  = 'https://nominatim.openstreetmap.org'
+OVERPASS_API   = 'https://overpass-api.de/api/interpreter'
+OSM_API        = 'https://www.openstreetmap.org/api/0.6'
+OSM_URI_PREFIX = 'https://www.openstreetmap.org'
 
+DATA_DIR=realpath(join(dirname(__file__), '../data'))
 
-US_MAP = {
-    'US'                   : 'relation/148838',
-    'Alabama'              : 'relation/161950',
-    'Alaska'               : 'relation/1116270',
-    'Arizona'              : 'relation/162018',
-    'Arkansas'             : 'relation/161646',
-    'California'           : 'relation/165475',
-    'Colorado'             : 'relation/161961',
-    'Connecticut'          : 'relation/165794',
-    'Delaware'             : 'relation/162110',
-    'Florida'              : 'relation/162050',
-    'Georgia'              : 'relation/161957',
-    'Hawaii'               : 'relation/166563',
-    'Idaho'                : 'relation/162116',
-    'Illinois'             : 'relation/122586',
-    'Indiana'              : 'relation/161816',
-    'Iowa'                 : 'relation/161650',
-    'Kansas'               : 'relation/161644',
-    'Kentucky'             : 'relation/161655',
-    'Louisiana'            : 'relation/224922',
-    'Maine'                : 'relation/63512',
-    'Maryland'             : 'relation/162112',
-    'Massachusetts'        : 'relation/61315',
-    'Michigan'             : 'relation/165789',
-    'Minnesota'            : 'relation/165471',
-    'Mississippi'          : 'relation/161943',
-    'Missouri'             : 'relation/161638',
-    'Montana'              : 'relation/162115',
-    'Nebraska'             : 'relation/161648',
-    'Nevada'               : 'relation/165473',
-    'New Hampshire'        : 'relation/67213',
-    'New Jersey'           : 'relation/224951',
-    'New Mexico'           : 'relation/162014',
-    'New York'             : 'relation/61320',
-    'North Carolina'       : 'relation/224045',
-    'North Dakota'         : 'relation/161653',
-    'Ohio'                 : 'relation/162061',
-    'Oklahoma'             : 'relation/161645',
-    'Oregon'               : 'relation/165476',
-    'Pennsylvania'         : 'relation/162109',
-    'Rhode Island'         : 'relation/392915',
-    'South Carolina'       : 'relation/224040',
-    'South Dakota'         : 'relation/161652',
-    'Tennessee'            : 'relation/161838',
-    'Texas'                : 'relation/114690',
-    'Utah'                 : 'relation/161993',
-    'Vermont'              : 'relation/60759',
-    'Virginia'             : 'relation/224042',
-    'Washington'           : 'relation/165479',
-    'West Virginia'        : 'relation/162068',
-    'Wisconsin'            : 'relation/165466',
-    'Wyoming'              : 'relation/161991',
-    'District of Columbia' : 'relation/162069'
+WORLD = {
+    'United States': (148838, {
+        'Alabama'              : 161950,
+        'Alaska'               : 1116270,
+        'Arizona'              : 162018,
+        'Arkansas'             : 161646,
+        'California'           : 165475,
+        'Colorado'             : 161961,
+        'Connecticut'          : 165794,
+        'Delaware'             : 162110,
+        'Florida'              : 162050,
+        'Georgia'              : 161957,
+        'Hawaii'               : 166563,
+        'Idaho'                : 162116,
+        'Illinois'             : 122586,
+        'Indiana'              : 161816,
+        'Iowa'                 : 161650,
+        'Kansas'               : 161644,
+        'Kentucky'             : 161655,
+        'Louisiana'            : 224922,
+        'Maine'                : 63512,
+        'Maryland'             : 162112,
+        'Massachusetts'        : 61315,
+        'Michigan'             : 165789,
+        'Minnesota'            : 165471,
+        'Mississippi'          : 161943,
+        'Missouri'             : 161638,
+        'Montana'              : 162115,
+        'Nebraska'             : 161648,
+        'Nevada'               : 165473,
+        'New Hampshire'        : 67213,
+        'New Jersey'           : 224951,
+        'New Mexico'           : 162014,
+        'New York'             : 61320,
+        'North Carolina'       : 224045,
+        'North Dakota'         : 161653,
+        'Ohio'                 : 162061,
+        'Oklahoma'             : 161645,
+        'Oregon'               : 165476,
+        'Pennsylvania'         : 162109,
+        'Rhode Island'         : 392915,
+        'South Carolina'       : 224040,
+        'South Dakota'         : 161652,
+        'Tennessee'            : 161838,
+        'Texas'                : 114690,
+        'Utah'                 : 161993,
+        'Vermont'              : 60759,
+        'Virginia'             : 224042,
+        'Washington'           : 165479,
+        'West Virginia'        : 162068,
+        'Wisconsin'            : 165466,
+        'Wyoming'              : 161991,
+        'District of Columbia' : 162069
+    }),
+    'Austria'     : 16239,
+    'Belgium'     : 52411,
+    'Bulgaria'    : 186382,
+    'Croatia'     : 214885,
+    'Cyprus'      : 307787,
+    'Czechia'     : 51684,
+    'Denmark'     : 50046,
+    'Estonia'     : 79510,
+    'Finland'     : 54224,
+    'France'      : 2202162,
+    'Germany'     : 51477,
+    'Greece'      : 192307,
+    'Hungary'     : 21335,
+    'Ireland'     : 62273,
+    'Italy'       : 365331,
+    'Latvia'      : 72594,
+    'Lithuania'   : 72596,
+    'Luxembourg'  : 2171347,
+    'Malta'       : 365307,
+    'Netherlands' : 2323309,
+    'Poland'      : 49715,
+    'Portugal'    : 295480,
+    'Romania'     : 90689,
+    'Slovakia'    : 14296,
+    'Slovenia'    : 218657,
+    'Spain'       : 1311341,
+    'Sweden'      : 52822
 }
 
 
@@ -77,25 +109,15 @@ def query_overpass(query: str, timeout: float | None):
     return res.json()
 
 
+def fetch_osm_xml(id: str):
+    res = requests.get(f'{OSM_API}/{id}')
+    res.raise_for_status()
+    return res.text
+
+
 def search_overpass_by_id(id: str, timeout=25):
     type_, id = id.split('/')
     return query_overpass(f'{type_}({id})', timeout)
-
-
-def download_us_map(dir=realpath(join(dirname(__file__), '../data/us'))):
-    for key, id in US_MAP.items():
-        name = key.lower().replace(' ', '-')
-
-        pathname = f'{join(dir, name)}.geojson'
-        with open(pathname, 'wt') as f:
-            print(f"Downloading {key} boundary to {pathname}...", end='')
-            sys.stdout.flush()
-
-            jsn = search_overpass_by_id(id)
-            gjsn = osm2geojson.json2geojson(jsn)
-            f.write(json.dumps(gjsn, indent=4))
-
-            print("done.")
 
 
 def extract_boundary(obj: dict):
@@ -117,12 +139,44 @@ def extract_boundary(obj: dict):
 
         with suppress(KeyError): attrs['country'] = tags['ISO3166-1']
         with suppress(KeyError): attrs['state'] = tags['ISO3166-2']
-        with suppress(KeyError): attrs['name'] = tags['name']
+
+        try:
+            attrs['name'] = tags['name:en']
+        except KeyError:
+            with suppress(KeyError):
+                attrs['name'] = tags['name']
 
         return feature['geometry'], attrs
 
     raise Exception('No Feature with type relation found')
 
 
+def fetch_from_osm(data, dir):
+    os.makedirs(dir, exist_ok=True)
+    for name, node in data.items():
+        if isinstance(node, tuple) or isinstance(node, list):
+            id, *rest = node
+            children = rest[0] if len(rest) == 1 else {}
+        else:
+            id = node
+            children = {}
+
+        print(f"Downloading {name}...", end='')
+        sys.stdout.flush()
+        gjsn = osm2geojson.json2geojson(search_overpass_by_id(f'relation/{id}'))
+
+        _, attrs = extract_boundary(gjsn)
+        name = attrs['name'].lower().replace(" ", "-")
+        filename = f'{name}.geojson'
+
+        path = join(dir, filename)
+        with open(path, 'wt') as f:
+            f.write(json.dumps(gjsn, indent=4))
+        print(f"{path}")
+
+        if children:
+            fetch_from_osm(children, join(dir, name))
+
+
 if __name__ == '__main__':
-    download_us_map()
+    fetch_from_osm(WORLD, join(DATA_DIR, 'world'))
